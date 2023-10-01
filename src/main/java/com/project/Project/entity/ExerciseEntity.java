98609944e93d31c439543405b28a699d44d566e8
yablonya <122230172+yablonya@users.noreply.github.com>
2023-10-01 17:45:59 +0300

    added all exercise pictures and complete project

--- src/main/java/com/project/Project/entity/ExerciseEntity.java
+++ src/main/java/com/project/Project/entity/ExerciseEntity.java
@@ -6,9 +6,9 @@ import lombok.Data;
 public class ExerciseEntity {
     private final String name;
     private final Integer[] repetitions;
-    private final byte[] image;
+    private final String image;
 
-    public ExerciseEntity(String name, Integer[] repetitions, byte[] image) {
+    public ExerciseEntity(String name, Integer[] repetitions, String image) {
         this.name = name;
         this.repetitions = repetitions;
         this.image = image;
--- src/main/java/com/project/Project/repositories/DataInitializer.java
+++ src/main/java/com/project/Project/repositories/DataInitializer.java
@@ -4,11 +4,11 @@ import com.project.Project.entity.ExerciseEntity;
 import com.project.Project.entity.TrainingProgramEntity;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
@@ -19,77 +19,77 @@ public class DataInitializer {
 
     @PostConstruct
     public void initializeDate() {
-        TrainingProgramEntity chest = new TrainingProgramEntity("Груди");
-        TrainingProgramEntity back = new TrainingProgramEntity("Спина");
-        TrainingProgramEntity legs = new TrainingProgramEntity("Ноги");;
-        TrainingProgramEntity cardio = new TrainingProgramEntity("Кардіо");;
+        TrainingProgramEntity chest = new TrainingProgramEntity("Chest");
+        TrainingProgramEntity back = new TrainingProgramEntity("Back");
+        TrainingProgramEntity legs = new TrainingProgramEntity("Legs");
+        TrainingProgramEntity cardio = new TrainingProgramEntity("Cardio");
+        String BASE_IMAGE_URL = "src/main/resources/exercise-images";
+
         chest.getExercises().addAll(Arrays.asList(
-                exerciseFactory("Жим лежачи", 10, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Жим штанги під кутом", 12, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Метелик", 20, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Жим гантелей на горизонтальній лавці", 12, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Розведення під кутом", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Жим гантелей сидячи", 12, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Махи в сторони", 20, 3,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Махи перед собою", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg")
+                exerciseFactory("Bench press", 10, 4,
+                        BASE_IMAGE_URL + "/chest/bench-press.jpg"),
+                exerciseFactory("Angled barbell press", 12, 4,
+                        BASE_IMAGE_URL + "/chest/angle-bench-press.jpg"),
+                exerciseFactory("Butterfly", 20, 4,
+                        BASE_IMAGE_URL + "/chest/butterfly.jpg"),
+                exerciseFactory("Dumbbell press on a horizontal bench", 12, 4,
+                        BASE_IMAGE_URL + "/chest/dumbbell-press.jpg"),
+                exerciseFactory("Spreading hands at an angle", 15, 4,
+                        BASE_IMAGE_URL + "/chest/spreads.jpg"),
+                exerciseFactory("Seated dumbbell press", 12, 4,
+                        BASE_IMAGE_URL + "/chest/sitting-dumbbell-press.jpg"),
+                exerciseFactory("Wave your hands to the sides", 20, 3,
+                        BASE_IMAGE_URL + "/chest/sides-swings.jpg"),
+                exerciseFactory("Wave your hands in front of you", 15, 4,
+                        BASE_IMAGE_URL + "/chest/front-swings.jpg")
         ));
         back.getExercises().addAll(Arrays.asList(
-                exerciseFactory("Тяга блоку до поясу сидячи", 20, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Рижачна тяга сидячи", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Пуловер з гантелею", 12, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Тяга штанги під нахилом", 10, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Рижачна тяга зверху", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Тяга блоку перед головою", 20, 3,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Махи назад під нахилом", 20, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Шраги", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg")
+                exerciseFactory("Pulling the block to the waist while sitting", 20, 4,
+                        BASE_IMAGE_URL + "/back/sitting-block-pull.jpg"),
+                exerciseFactory("Linkage pull while sitting", 15, 4,
+                        BASE_IMAGE_URL + "/back/horizontal-cravings.jpeg"),
+                exerciseFactory("Pullover with a dumbbell", 12, 4,
+                        BASE_IMAGE_URL + "/back/dumbbell-pullover.jpg"),
+                exerciseFactory("Inclined barbell pull", 10, 4,
+                        BASE_IMAGE_URL + "/back/angle-barbell-pull.jpg"),
+                exerciseFactory("Linkage pull from upside", 15, 4,
+                        BASE_IMAGE_URL + "/back/vertical-cravings.jpeg"),
+                exerciseFactory("Block traction in front of the head", 20, 3,
+                        BASE_IMAGE_URL + "/back/block-pull.jpg"),
+                exerciseFactory("Swing back at an angle", 20, 4,
+                        BASE_IMAGE_URL + "/back/angle-back-swings.jpg"),
+                exerciseFactory("Shoulder shrug", 15, 4,
+                        BASE_IMAGE_URL + "/back/shoulder-shrug.jpg")
         ));
         legs.getExercises().addAll(Arrays.asList(
-                exerciseFactory("Розгинання сидячи", 20, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Жим однією ногою", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Присідання зі штангою", 10, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Випади в штанзі", 12 , 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Згинання лежачи", 20, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Згинання сидячи", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Екстензія", 12, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Голінь сидячи", 30, 3,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Голінь стоячи", 15, 4,
-                        "src/main/resources/exercise-images/bench-press.jpg")
+                exerciseFactory("Stretching while sitting", 20, 4,
+                        BASE_IMAGE_URL + "/legs/sitting-stretch.jpg"),
+                exerciseFactory("Leg press", 15, 4,
+                        BASE_IMAGE_URL + "/legs/leg-press.jpg"),
+                exerciseFactory("Barbell squats", 10, 4,
+                        BASE_IMAGE_URL + "/legs/barbell-squats.jpg"),
+                exerciseFactory("Lunges", 12 , 4,
+                        BASE_IMAGE_URL + "/legs/lunges.jpg"),
+                exerciseFactory("Lying flexion", 20, 4,
+                        BASE_IMAGE_URL + "/legs/lying-flexion.jpg"),
+                exerciseFactory("Sitting flexion", 15, 4,
+                        BASE_IMAGE_URL + "/legs/sitting-flexion.jpg"),
+                exerciseFactory("Extension", 12, 4,
+                        BASE_IMAGE_URL + "/legs/extension.jpg"),
+                exerciseFactory("Shin standing", 15, 4,
+                        BASE_IMAGE_URL + "/legs/shin-standing.jpg")
         ));
         cardio.getExercises().addAll(Arrays.asList(
-                exerciseFactory("Ходьба на біговій доріжці", 5,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Біг з нагрузкою", 5,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Степер", 5,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Велотренажер", 5,
-                        "src/main/resources/exercise-images/bench-press.jpg"),
-                exerciseFactory("Велотренажер з легким темпом", 5 ,
-                        "src/main/resources/exercise-images/bench-press.jpg")
+                exerciseFactory("Walking on a treadmill", 5,
+                        BASE_IMAGE_URL + "/cardio/treadmill.jpg"),
+                exerciseFactory("Running with load", 5,
+                        BASE_IMAGE_URL + "/cardio/treadmill.jpg"),
+                exerciseFactory("Stepper", 5,
+                        BASE_IMAGE_URL + "/cardio/stepper.jpg"),
+                exerciseFactory("Exercise bike", 5,
+                        BASE_IMAGE_URL + "/cardio/exercise-bike.jpg"),
+                exerciseFactory("Exercise bike with an easy pace", 5 ,
+                        BASE_IMAGE_URL + "/cardio/exercise-bike.jpg")
         ));
 
         programRepository.addProgram(chest);
@@ -106,9 +106,10 @@ public class DataInitializer {
         return new ExerciseEntity(name, new Integer[]{minutes}, imageToBytes(imagePath));
     }
 
-    private byte[] imageToBytes(String imagePath) {
+    private String  imageToBytes(String imagePath) {
         try {
-            return Files.readAllBytes(Paths.get(imagePath));
+            byte[] bytesFromImage = Files.readAllBytes(Paths.get(imagePath));
+            return Base64.encodeBase64String(bytesFromImage);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
